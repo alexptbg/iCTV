d487d70a4361d71f84042d5dcad2b3fa7ebe9f7b
Alex Soares <alexptbg@gmail.com>
2017-01-30 11:50:41 +0200

    Database uploaded

--- inc/db.php
+++ inc/db.php
@@ -2,8 +2,8 @@
 defined('start') or die('Direct access not allowed.');
 define("DB_SERVER","localhost");
 define("DB_NAME","imdb");
-define("DB_USER","root");
-define("DB_PASS","11543395");
+define("DB_USER","");
+define("DB_PASS","");
 $connection = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
 if (!$connection) { die("Database connection failed" .mysql_error()); } 
 mysql_query("SET NAMES 'utf8'");
